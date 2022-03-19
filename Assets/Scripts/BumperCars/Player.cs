072f89ea0b6af401a84484d3e2ce61fc289b641f
tomaszmakowski <makowskit@student.agh.edu.pl>
2022-03-19 17:08:25 +0100

    joystick added

--- Assets/Scripts/BumperCars/Player.cs
+++ Assets/Scripts/BumperCars/Player.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using static System.Math;
 
+
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 0.02f; 
     protected Vector3 _playerMove;
 
     private BoxCollider2D _boxCollider2D;
-    
-    
 
-    public float Velocity = 1.0f;
+    private joystickScript _joystick;
+
+    //public float Velocity = 1.0f;
 
     private void Awake()
     {
         _boxCollider2D = GetComponent<BoxCollider2D>();
+        _joystick = GameObject.Find("JoystickCircleDirection").GetComponent<joystickScript>();
     }
     // Start is called before the first frame update
     void Start()
@@ -26,7 +29,7 @@ public class Player : MonoBehaviour
     private void Move(Vector3 input)
     {
         float degrees = (float) Atan2(input.x, input.y);
-        _playerMove = new Vector3(input.x, input.y, 0).normalized * Velocity;
+        _playerMove = new Vector3(input.x, input.y, 0);
         transform.Translate(_playerMove);
         // transform.rotation = Quaternion.Euler(new Vector3(0, 0, degrees));
 
@@ -36,8 +39,9 @@ public class Player : MonoBehaviour
     
     private void FixedUpdate()
     {
+        Vector2 input = _joystick.getValue() * speed;
         float y = Input.GetAxisRaw("Vertical");
         float x = Input.GetAxisRaw("Horizontal");
-        Move(new Vector3(x, y, 0));
+        Move(new Vector3(input.x, input.y, 0));
     }
 }
