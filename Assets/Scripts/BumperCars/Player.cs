b613fb1f77825363060e57ed168d0c3d2fcf1ebc
corey888773 <piotrgasiorek773@gmail.com>
2022-03-19 17:51:14 +0100

    collision-added

--- Assets/Scripts/BumperCars/Player.cs
+++ Assets/Scripts/BumperCars/Player.cs
@@ -6,15 +6,17 @@ using static System.Math;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed = 10.0f; 
+     
     protected Vector2 _playerMove;
 
     private BoxCollider2D _boxCollider2D;
+    protected RaycastHit2D hit;
 
     private joystickScript _joystick;
     
-    public float rotationSpeed = 720.0f;
-
+    public float _rotationSpeed = 720.0f;
+    public float _velocity = 10.0f;
+    
     private void Awake()
     {
         _boxCollider2D = GetComponent<BoxCollider2D>();
@@ -31,13 +33,29 @@ public class Player : MonoBehaviour
         float degrees = (float) Atan2(input.x, input.y);
         _playerMove = new Vector2(input.x, input.y);
         float inputMagnitude = Mathf.Clamp01(_playerMove.magnitude);
-        transform.Translate(_playerMove * Time.deltaTime, Space.World);
+        // transform.Translate(_playerMove * Time.deltaTime, Space.World);
 
 
         if (_playerMove != Vector2.zero)
         {
             Quaternion rotateDirection= Quaternion.LookRotation(Vector3.forward, _playerMove);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDirection, Time.deltaTime * rotationSpeed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDirection, Time.deltaTime * _rotationSpeed);
+        }
+        
+        
+        // move collision test. box creates a box on top of our object which is moved slightly into the movement direction before the player.
+        //the box tests if it covers any colliders, and if so, it stops the movement.
+        hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size,0, new Vector2(0,_playerMove.y),
+            Mathf.Abs(_playerMove.y * Time.deltaTime), LayerMask.GetMask("Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(new Vector3(0, _playerMove.y * Time.deltaTime * inputMagnitude, 0), Space.World);
+        }
+        hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size, 0, new Vector2(_playerMove.x, 0),
+            Mathf.Abs(_playerMove.x * Time.deltaTime), LayerMask.GetMask("Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(new Vector3(_playerMove.x * Time.deltaTime * inputMagnitude,0 , 0), Space.World);
         }
 
 
@@ -45,7 +63,7 @@ public class Player : MonoBehaviour
     
     private void FixedUpdate()
     {
-        Vector2 input = _joystick.getValue() * speed;
+        Vector2 input = _joystick.getValue() * _velocity;
         float y = Input.GetAxisRaw("Vertical");
         float x = Input.GetAxisRaw("Horizontal");
         Move(new Vector2(input.x, input.y));
