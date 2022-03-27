90a9655f97a060d85a0fa1a024184f723e47d6b5
corey888773 <piotrgasiorek773@gmail.com>
2022-03-27 18:40:28 +0200

    border

--- Assets/Scripts/BumperCars/Player.cs
+++ Assets/Scripts/BumperCars/Player.cs
@@ -46,13 +46,13 @@ public class Player : MonoBehaviour
         // move collision test. box creates a box on top of our object which is moved slightly into the movement direction before the player.
         //the box tests if it covers any colliders, and if so, it stops the movement.
         hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size,0, new Vector2(0,_playerMove.y),
-            Mathf.Abs(_playerMove.y * Time.deltaTime), LayerMask.GetMask("Blocking"));
+            Mathf.Abs(_playerMove.y * Time.deltaTime ), LayerMask.GetMask("Blocking"));
         if (hit.collider == null)
         {
             transform.Translate(new Vector3(0, _playerMove.y * Time.deltaTime * inputMagnitude, 0), Space.World);
         }
         hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size, 0, new Vector2(_playerMove.x, 0),
-            Mathf.Abs(_playerMove.x * Time.deltaTime), LayerMask.GetMask("Blocking"));
+            Mathf.Abs(_playerMove.x * Time.deltaTime ), LayerMask.GetMask("Blocking"));
         if (hit.collider == null)
         {
             transform.Translate(new Vector3(_playerMove.x * Time.deltaTime * inputMagnitude,0 , 0), Space.World);
