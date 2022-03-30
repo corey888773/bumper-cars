f7c8a0030fddf0651fa7fec138c48387d5f9857c
tomaszmakowski <makowskit@student.agh.edu.pl>
2022-03-30 16:58:01 +0200

    rigidbody

--- Assets/Scripts/BumperCars/Player.cs
+++ Assets/Scripts/BumperCars/Player.cs
@@ -10,7 +10,9 @@ public class Player : MonoBehaviour
     protected Vector2 _playerMove;
 
     private BoxCollider2D _boxCollider2D;
+    private Rigidbody2D _rigidbody2D;
     protected RaycastHit2D hit;
+    
 
     private joystickScript _joystick;
     
@@ -20,6 +22,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         _boxCollider2D = GetComponent<BoxCollider2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         _joystick = GameObject.Find("JoystickCircleDirection").GetComponent<joystickScript>();
     }
     // Start is called before the first frame update
@@ -43,22 +46,24 @@ public class Player : MonoBehaviour
         }
         
         
-        // move collision test. box creates a box on top of our object which is moved slightly into the movement direction before the player.
-        //the box tests if it covers any colliders, and if so, it stops the movement.
-        hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size,0, new Vector2(0,_playerMove.y),
-            Mathf.Abs(_playerMove.y * Time.deltaTime ), LayerMask.GetMask("Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(new Vector3(0, _playerMove.y * Time.deltaTime * inputMagnitude, 0), Space.World);
-        }
-        hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size, 0, new Vector2(_playerMove.x, 0),
-            Mathf.Abs(_playerMove.x * Time.deltaTime ), LayerMask.GetMask("Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(new Vector3(_playerMove.x * Time.deltaTime * inputMagnitude,0 , 0), Space.World);
-        }
-
-
+        // // move collision test. box creates a box on top of our object which is moved slightly into the movement direction before the player.
+        // //the box tests if it covers any colliders, and if so, it stops the movement.
+        // hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size,0, new Vector2(0,_playerMove.y),
+        //     Mathf.Abs(_playerMove.y * Time.deltaTime ), LayerMask.GetMask("Blocking"));
+        // if (hit.collider == null)
+        // {
+        //     transform.Translate(new Vector3(0, _playerMove.y * Time.deltaTime * inputMagnitude, 0), Space.World);
+        // }
+        // hit = Physics2D.BoxCast(transform.position, _boxCollider2D.size, 0, new Vector2(_playerMove.x, 0),
+        //     Mathf.Abs(_playerMove.x * Time.deltaTime ), LayerMask.GetMask("Blocking"));
+        // if (hit.collider == null)
+        // {
+        //     transform.Translate(new Vector3(_playerMove.x * Time.deltaTime * inputMagnitude,0 , 0), Space.World);
+        // }
+            
+        // transform.Translate(_playerMove * Time.deltaTime * inputMagnitude, Space.World);
+        _rigidbody2D.AddForce(_playerMove * inputMagnitude);
+        
     }
     
     private void FixedUpdate()
