6b6e697aac5c0bfec272b9dd729d204d2696bf6a
Tosiek <antonizieciak@gmail.com>
2022-04-06 18:14:04 +0200

    10 obstacles!

--- Assets/Scripts/Czolgi/ObstacleGenerator.cs
+++ Assets/Scripts/Czolgi/ObstacleGenerator.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject obst;
+    private Camera cam;
+    private float cameraWidth,cameraHeight;
+    
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main; 
+        cameraHeight = cam.orthographicSize;
+        cameraWidth = Screen.width/Screen.height * cam.orthographicSize;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(obst, new Vector3(Random.Range(-cameraWidth, cameraWidth),
+                Random.Range(-cameraHeight, cameraHeight), 0), Quaternion.identity);
+        }
     }
 }
