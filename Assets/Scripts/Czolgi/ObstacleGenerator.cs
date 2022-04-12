baa7891c4d6731a9a2ed4f7e2d2de20969989072
Tosiek <antonizieciak@gmail.com>
2022-04-12 17:23:08 +0200

    Randomly spawned obstacles

--- Assets/Scripts/Czolgi/ObstacleGenerator.cs
+++ Assets/Scripts/Czolgi/ObstacleGenerator.cs
@@ -12,11 +12,17 @@ public class ObstacleGenerator : MonoBehaviour
         cam = Camera.main; 
         cameraHeight = cam.orthographicSize;
         cameraWidth = Screen.width/Screen.height * cam.orthographicSize;
-        
-        for (int i = 0; i < 10; i++)
+
+        var horSecSize = cameraWidth / 2;
+        var verSecSize = cameraHeight / 2;
+
+        for (int i = 0; i < 4; i++)
         {
-            Instantiate(obst, new Vector3(Random.Range(-cameraWidth, cameraWidth),
-                Random.Range(-cameraHeight, cameraHeight), 0), Quaternion.identity);
+            for (int j = 0; j < 4; j++)
+            {
+                Instantiate(obst, new Vector3(Random.Range(-cameraWidth+i*horSecSize, -cameraWidth+(i+1)*horSecSize-0.7f),
+                    Random.Range(-cameraHeight+j*verSecSize, -cameraHeight+(j+1)*verSecSize-0.7f), 0), Quaternion.identity);
+            }
         }
     }
 }
