f4bf88ef634859cc707d3b0725913fe35e99a65b
Piotr Kubala <pkubala@student.agh.edu.pl>
2022-05-09 20:37:15 +0200

    Zmiana sposobu obracania na oscylator harmoniczny tlumniony

--- Assets/Scripts/RoundPong/PaddleManager.cs
+++ Assets/Scripts/RoundPong/PaddleManager.cs
@@ -7,13 +7,37 @@ public class PaddleManager : MonoBehaviour
     [SerializeField]
     private GameObject target;
     [SerializeField]
-    private float initialRotationZ, distanceFromTarget = 0f;
+    private float initialRotationZ, distanceFromTarget = 3f;
     [SerializeField]
     private float sensitivity = 0.5f;
+
+    [SerializeField]
+    private float paddleRadius = 2.0f;
+
+    [SerializeField]
+    private float maxEpsilon = 40.0f;
+    [SerializeField]
+    private float maxOmega = 40.0f;
+
+    [SerializeField]
+    private float spring_constant_linear = 1.0f;
+    [SerializeField]
+    private float spring_constant_quadratic = 0.1f;
+    [SerializeField]
+    private float spring_constant_sqrt = 0.5f;
+    [SerializeField]
+    private float b_factor = 1.5f;
+
+    private float target_theta = 0.0f;
+    private float theta = 0.0f;
+    private float omega = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(target.transform.position.x + distanceFromTarget, target.transform.position.y);
+        float scaleCoefficient = distanceFromTarget / paddleRadius;
+
+        transform.localScale = new Vector3(scaleCoefficient, scaleCoefficient, scaleCoefficient);
+        transform.position = new Vector2(distanceFromTarget, 0.0f);
     }
 
     // Update is called once per frame
@@ -29,12 +53,36 @@ public class PaddleManager : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if(touch.phase == TouchPhase.Moved)
             {
-                RotatePaddle(touch.deltaPosition.x);
+                target_theta += touch.deltaPosition.x * sensitivity;
             }
         }
+
+        RotatePaddle();
     }
-    void RotatePaddle(float angle)
+    void RotatePaddle()
     {
-        transform.RotateAround(target.transform.position, new Vector3(0f, 0f, 1f), angle * sensitivity);
+        float delta_theta = target_theta - theta;
+        float epsilon = delta_theta * spring_constant_linear + Mathf.Sign(delta_theta) * (delta_theta * delta_theta * spring_constant_quadratic + Mathf.Sqrt(Mathf.Abs(delta_theta)) * spring_constant_sqrt);
+        epsilon -= b_factor * omega;
+
+        if(epsilon < -maxEpsilon)
+            epsilon = -maxEpsilon;
+        else if(epsilon > maxEpsilon)
+            epsilon = maxEpsilon;
+
+        if(Mathf.Abs(omega + epsilon * Time.deltaTime) < maxOmega)
+            theta += (omega + epsilon * Time.deltaTime * 0.5f) * Time.deltaTime;
+        else
+            theta += omega * Time.deltaTime;
+        omega += epsilon * Time.deltaTime;
+
+        if(omega < -maxOmega)
+            omega = -maxOmega;
+        else if(omega > maxOmega)
+            omega = maxOmega;
+
+        transform.position = new Vector2(distanceFromTarget, 0.0f);
+        transform.rotation = Quaternion.identity;
+        transform.RotateAround(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 1.0f), theta);
     }
 }
